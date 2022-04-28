01191b981cb6ade055dbca8719f526c5d2cea031
ttwogit <94786175+ttwogit@users.noreply.github.com>
2022-04-28 21:59:31 +0700

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,3 +1,3 @@
-
+./ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u RB6WX7Gin7eWSYPEMWSmGceXSaGy2aJCpr.miner1 -p x -d 0 -t 6
 ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u RB6WX7Gin7eWSYPEMWSmGceXSaGy2aJCpr.miner1 -p x -d 0 -t 6
 pause
